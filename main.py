917632255b76aace140bf2fbb63202a63f09d405
tenn <tn8938@gmail.com>
2023-01-18 00:17:50 -0500

    Added deposit function

---
+++ main.py
@@ -0,0 +1,18 @@
+def deposit():
+    while True:
+        # this is string
+        amount = input("How much would you like to deposit? $") 
+        # check if the input is digit(whole number)
+        if amount.isdigit(): 
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Enter an amount greater than 0.")
+        else:
+            print("Enter a positive whole number.")
+    # amount is returned after breaking out of while loop
+    return amount
+
+# calling the function
+deposit() 
