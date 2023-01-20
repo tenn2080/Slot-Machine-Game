32b8e974b973b5abac123ed79cba092bcd96c1be
tenn <tn8938@gmail.com>
2023-01-19 23:35:20 -0500

    added function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+MAX_LINES = 3
+
 def deposit_money():
     while True:
         # this is string
@@ -14,5 +16,24 @@ def deposit_money():
     # amount is returned after breaking out of while loop
     return amount
 
+def get_num_lines():
+    while True:
+        lines = input(f"How many lines do you want play? enter between 1 - {MAX_LINES} ") 
+        if lines.isdigit(): 
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter an amount between 1-3")
+        else:
+            print("Enter a positive whole number. between 1-3")
+    
+    return lines
+
 # calling the function
-deposit_money() 
+def main():
+    balance = deposit_money()
+    lines = get_num_lines()
+    print(balance, lines)
+
+main()
